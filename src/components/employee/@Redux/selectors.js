67dd49f8b424650ad5c36423ea0f8f3f6fc6439e
subhiksha_lakshmi <gsubhikshalakshmi@gmail.com>
2022-11-23 16:56:43 +0530

    feat(EmployeeTree): Org tree based on filtered val

--- src/components/employee/@Redux/selectors.js
+++ src/components/employee/@Redux/selectors.js
@@ -8,26 +8,55 @@ export const selectEmployeeDetailsToDisplay = createSelector(selectEmployeeDetai
   (employeeDetails, searchValue, filterValue) => {
     const searchedEmployeeDetails = searchValue ? employeeDetails.filter(employee => 
       employee.name.toLowerCase().includes(searchValue) || employee.designation.toLowerCase().includes(searchValue) || employee.team.toLowerCase().includes(searchValue)) : employeeDetails;
-    const filteredEmployeDetails = filterValue ? searchedEmployeeDetails.filter(employee => employee.team.toLowerCase() === filterValue.toLowerCase()) : searchedEmployeeDetails;
-    return filteredEmployeDetails;
+    const filteredEmployeeDetails = filterValue ? searchedEmployeeDetails.filter(employee => employee.team.toLowerCase() === filterValue.toLowerCase()) : searchedEmployeeDetails;
+    return filteredEmployeeDetails;
 });
 
-export const selectEmployeeTree = createSelector(selectEmployeeDetails,
-  (employeeDetailsToDisplay) => {
+export const selectEmployeeTree = createSelector(selectEmployeeDetails, selectFilterValue,
+  (employeeDetailsPresent, filterValue) => {
     let root = {};
-    if (employeeDetailsToDisplay) {
-      const idMapping = employeeDetailsToDisplay.reduce((acc, el, i) => {
+    if (employeeDetailsPresent) {
+      let filteredEmployeeToDisplay = [];
+      employeeDetailsPresent.forEach((el) => {
+        if (el.children) {
+          delete el.children;
+        } 
+      });
+    if (filterValue) {
+      const filteredEmployeeDetails = employeeDetailsPresent.filter(employee => employee.team.toLowerCase() === filterValue.toLowerCase());
+      
+      filteredEmployeeDetails.forEach(filterEmployee => {
+        let managerId = filterEmployee.managerId;
+        const isEmployeePresent = filteredEmployeeToDisplay.some(emp => {
+          return  emp.id === filterEmployee.id;
+        });
+        if (!isEmployeePresent) {
+          filteredEmployeeToDisplay.push(filterEmployee);
+        }
+        while (managerId !== null) {
+          const employeeFound = employeeDetailsPresent.find(emp => emp.id === managerId);
+          const isEmployeePresentInFilterArray = filteredEmployeeToDisplay.some(emp => emp.id === employeeFound.id);
+          if (!isEmployeePresentInFilterArray) {
+            filteredEmployeeToDisplay.push(employeeFound);
+          }
+          managerId = employeeFound.managerId;
+        }
+      });
+    }
+
+    const employeeDetailsTree = filterValue ? filteredEmployeeToDisplay: employeeDetailsPresent; 
+      const idMapping = employeeDetailsTree.reduce((acc, el, i) => {
         acc[el.id] = i;
         return acc;
       }, {});
-      employeeDetailsToDisplay.forEach((el) => {
+      employeeDetailsTree.forEach((el) => {
         // Handle the root element
         if (el.managerId === null) {
           root = el;
           return;
         }
         // Use our mapping to locate the parent element in our data array
-        const parentEl = employeeDetailsToDisplay[idMapping[el.managerId]];
+        const parentEl = employeeDetailsTree[idMapping[el.managerId]];
         // Add our current el to its parent's `children` array
         parentEl.children = [...(parentEl.children || []), el];
       });
--- src/components/employee/EmployeeTree.js
+++ src/components/employee/EmployeeTree.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectEmployeeTree } from './@Redux/selectors';
+import { selectEmployeeTree, selectFilterValue } from './@Redux/selectors';
 import OrganizationChart from '../employee-chart/ChartContainer';
 
 const EmployeeTree = () => {
   const employeeDetails = useSelector(state => selectEmployeeTree(state));
+  const isFilteredValue = useSelector(state => selectFilterValue(state));
   const dispatch = useDispatch();
 
   const updateEmployeeManager = (employeeId, managerId) => {
